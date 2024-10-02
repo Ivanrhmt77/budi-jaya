56e16ae40b3b375f32974872f863095af6a53466
Nama Anda <ivanrahmat.p77@gmail.com>
2024-10-02 11:07:10 +0700

    First commit

---
+++ src/test/java/com/company/budi_jaya/BudiJayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.budi_jaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudiJayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
